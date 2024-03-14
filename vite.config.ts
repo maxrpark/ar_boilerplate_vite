09c3b936477bdde4d671835ae66ef351a47a969f
maxirpark <maxirutipark@gmail.com>
2024-03-14 14:19:40 +0900

    GLTF

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default {
 
   resolve: {
     alias: {
-      "mindar-image-three":
-        "https://cdn.jsdelivr.net/npm/mind-ar@1.2.2/dist/mindar-image-three.prod.js",
       three: "https://unpkg.com/three@0.160.0/build/three.module.js",
       "three/addons/": "https://unpkg.com/three@0.160.0/examples/jsm/",
+      "mindar-image-three":
+        "https://cdn.jsdelivr.net/npm/mind-ar@1.2.2/dist/mindar-image-three.prod.js",
     },
   },
   assetsInclude: ["**/*.mind"],
